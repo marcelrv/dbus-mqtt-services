d79cb411a576739594308e9b1d8dfb1732b9137d
Marcel <marcel@verpaalen.com>
2025-03-20 09:41:58 +0100

    Update installrelease.sh

--- etc/dbus-mqtt-services/installrelease.sh
+++ etc/dbus-mqtt-services/installrelease.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -s https://api.github.com/repos/sebdehne/dbus-mqtt-services/releases/latest | grep "browser_download_url.*gz" | cut -d : -f 2,3 | tr -d \" | wget -O venus-data.tar.gz -qi -
+curl -s https://api.github.com/repos/marcelrv/dbus-mqtt-services/releases/latest | grep "browser_download_url.*gz" | cut -d : -f 2,3 | tr -d \" | wget -O venus-data.tar.gz -qi -
 tar -zxf ./venus-data.tar.gz -C /data
 sh /data/etc/dbus-mqtt-services/reinstalllocal.sh
